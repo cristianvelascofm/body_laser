b39cdfffb32f5cec7d2ac461ecaf5c93acd7c204
cristian.velasco <cristian.velasco@grandtek.co>
2022-03-16 21:55:56 -0500

    Product Tb -l

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -35,7 +35,7 @@
       ></v-checkbox>
 
       <div>
-      <v-btn color="#417683" small class="mr-4 white--text" @click="login; astro()"> Ingresar </v-btn>
+      <v-btn color="#417683" small class="mr-4 white--text" @click="login"> Ingresar </v-btn>
       </div>
     </v-form>
     <!-- </v-card> -->
