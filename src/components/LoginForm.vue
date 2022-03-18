28b7ea1af0882499520b6af1f901796521699d3d
cristian.velasco <cristian.velasco@grandtek.co>
2022-03-18 00:17:05 -0500

    Login Access

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -58,8 +58,8 @@ export default {
   
   data() {
     return {
-      // path: "http://192.168.56.1:5000/",
-      path : 'http://192.168.130.79:5000/',
+      path: "http://192.168.56.1:5000/",
+      // path : 'http://192.168.130.79:5000/',
       user_name: "",
       password: "",
       show_password: false,
@@ -86,28 +86,32 @@ export default {
     ...mapMutations(["login"]),
 
     encrypt(src){ 
-      encryptedText = this.$CryptoJS.AES.encrypt(src, "Secret Passphrase").toString()
-      return encryptedText
+      let encriptado = window.btoa(src)
+      return encriptado
     },
     decrypt(src){
-      decryptedText = this.$CryptoJS.AES.decrypt(src, "Secret Passphrase").toString(this.CryptoJS.enc.Utf8)
-      return decryptedText
+      let desencriptado = window.atob(src)
+      return desencriptado
     },
 
     login() {
-
-      encriptado = this.encrypt("Atlas")
-      console.log("Data: "+ encriptado)
-      // console.log('ok')
-      // var dict_data= {}
-      // dict_data ['action'] = 'login'
-      // dict_data ['user'] = this.user_name
-      // dict_data ['password'] = this.password
+      var dict_data= {}
+      var user_name_encript = this.encrypt(this.user_name)
+      var password_encript = this.encrypt(this.password)
+      dict_data ['action'] = 'login'
+      dict_data ['user_name'] = user_name_encript
+      dict_data ['password'] = password_encript
       
-      // axios.post(this.path, dict_data).then((response) => {
-      //   console.log(response)        
-      //   this.$store.commit('login')
-      // });
+      axios.post(this.path, dict_data).then((response) => {
+        console.log(response.data)
+        if (response.data.hasOwnProperty('error')){
+          console.log('Error')
+          
+        }else{
+          this.$store.commit('login')
+        }
+        
+      });
       
     },
   },
